fe2ce05d2146bb11ecc2ecfb31e52d12323a142a
pfohjo <pfohjo@gmail.com>
2013-12-09 17:21:56 +0100

    syscall bitmap from userspace

--- arch/x86/kvm/nitro_x86.c
+++ arch/x86/kvm/nitro_x86.c
@@ -3,10 +3,11 @@
 #include "x86.h"
 
 #include <linux/nitro_main.h>
+#include <linux/kernel.h>
 
 extern int kvm_set_msr_common(struct kvm_vcpu*, struct msr_data*);
 
-int nitro_set_syscall_trap(struct kvm *kvm){
+int nitro_set_syscall_trap(struct kvm *kvm,unsigned long *bitmap,int max_syscall){
   int i;
   struct kvm_vcpu *vcpu;
   u64 efer;
@@ -14,21 +15,25 @@ int nitro_set_syscall_trap(struct kvm *kvm){
   
   printk(KERN_INFO "nitro: set syscall trap\n");
   
-  mutex_lock(&kvm->lock);
+  kvm->nitro.syscall_bitmap = bitmap;
+  kvm->nitro.max_syscall = max_syscall;
+  
+  kvm->nitro.trap_syscall = 1;
   
   kvm_for_each_vcpu(i, vcpu, kvm){
-    vcpu_load(vcpu);
+    //vcpu_load(vcpu);
+    nitro_vcpu_load(vcpu);
+    
     kvm_get_msr_common(vcpu, MSR_EFER, &efer);
     msr_info.index = MSR_EFER;
     msr_info.data = efer & ~EFER_SCE;
     msr_info.host_initiated = true;
     kvm_set_msr_common(vcpu, &msr_info);
+    
+    INIT_COMPLETION(vcpu->nitro.k_wait_cv);
+    
     vcpu_put(vcpu);
   }
-  
-  kvm->nitro.trap_syscall = 1;
-  
-  mutex_unlock(&kvm->lock);
   
   return 0;
 }
@@ -41,38 +46,67 @@ int nitro_unset_syscall_trap(struct kvm *kvm){
   
   printk(KERN_INFO "nitro: unset syscall trap\n");
   
-  mutex_lock(&kvm->lock);
-  
-  kvm->nitro.trap_syscall = 0;
-  
   kvm_for_each_vcpu(i, vcpu, kvm){
-    vcpu_load(vcpu);
+    //vcpu_load(vcpu);
+    
+    vcpu->nitro.trap_syscall_hit = 0;
+    //if waiters, wake up
+    //if(completion_done(&(vcpu->nitro.k_wait_cv)) == 0)
+    complete_all(&(vcpu->nitro.k_wait_cv));
+    
+    
+    nitro_vcpu_load(vcpu);
+    
     kvm_get_msr_common(vcpu, MSR_EFER, &efer);
     msr_info.index = MSR_EFER;
     msr_info.data = efer | EFER_SCE;
     msr_info.host_initiated = true;
     kvm_set_msr_common(vcpu, &msr_info);
-    //if waiters, wake up
-    if(completion_done(&(vcpu->nitro.k_wait_cv)) == 0)
-      complete_all(&(vcpu->nitro.k_wait_cv));
+    
+
+    
     vcpu_put(vcpu);
   }
   
-  mutex_unlock(&kvm->lock);
+  kvm->nitro.trap_syscall = 0;
+  if(kvm->nitro.syscall_bitmap != NULL){
+    kfree(kvm->nitro.syscall_bitmap);
+    kvm->nitro.syscall_bitmap = NULL;
+  }
+  kvm->nitro.max_syscall = 0;
 
   return 0;
 }
 
 int nitro_handle_syscall_trap(struct kvm_vcpu *vcpu){
-  //printk(KERN_INFO "nitro: syscall trap\n");
+  unsigned long syscall_nr;
+  long rv;
+  struct kvm *kvm;
+  
+  kvm = vcpu->kvm;
   
   vcpu->nitro.trap_syscall_hit = 0;
+  
+  if(kvm->nitro.max_syscall > 0){
+    syscall_nr = kvm_register_read(vcpu, VCPU_REGS_RAX);
+    
+    if(syscall_nr > INT_MAX || syscall_nr > kvm->nitro.max_syscall || !test_bit((int)syscall_nr,kvm->nitro.syscall_bitmap))
+      return 1;
+  }
+  
+  
   vcpu->nitro.event = KVM_NITRO_SYSCALL_TRAPPED;
   
   up(&(vcpu->nitro.n_wait_sem));
-  vcpu_put(vcpu);
-  wait_for_completion_killable(&(vcpu->nitro.k_wait_cv));
-  vcpu_load(vcpu);
+  //vcpu_put(vcpu);
+  rv = wait_for_completion_interruptible_timeout(&(vcpu->nitro.k_wait_cv),msecs_to_jiffies(30000));
+  
+  if (rv == 0)
+    printk(KERN_INFO "nitro: %s: wait timed out\n",__FUNCTION__);
+  else if (rv < 0)
+    printk(KERN_INFO "nitro: %s: wait interrupted\n",__FUNCTION__);
+  
+  //vcpu_load(vcpu);
 
   //returning 0 will give control back to qemu
   return 1;
--- arch/x86/kvm/nitro_x86.h
+++ arch/x86/kvm/nitro_x86.h
@@ -3,7 +3,7 @@
 
 #include <linux/kvm_host.h>
 
-int nitro_set_syscall_trap(struct kvm*);
+int nitro_set_syscall_trap(struct kvm*,unsigned long*,int);
 int nitro_unset_syscall_trap(struct kvm*);
 
 int nitro_handle_syscall_trap(struct kvm_vcpu*);
