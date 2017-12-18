55d8cf1ff23d6475b02e45bf10b2c58fd7a9d06f
Adalbert Lazar <alazar@bitdefender.com>
2017-12-18 16:47:34 +0200

    kvm: x86: hook in kvmi_xsetbv_event()
    Notify the guest introspection tool that the extended control register
    has been changed.
    
    Signed-off-by: Mihai Donțu <mdontu@bitdefender.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -765,6 +765,9 @@ static int __kvm_set_xcr(struct kvm_vcpu *vcpu, u32 index, u64 xcr)
 
 int kvm_set_xcr(struct kvm_vcpu *vcpu, u32 index, u64 xcr)
 {
+	if (xcr != vcpu->arch.xcr0)
+		kvmi_xsetbv_event(vcpu);
+
 	if (kvm_x86_ops->get_cpl(vcpu) != 0 ||
 	    __kvm_set_xcr(vcpu, index, xcr)) {
 		kvm_inject_gp(vcpu, 0);
