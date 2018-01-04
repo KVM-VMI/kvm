2246dd5bab1fb71c41aab11c94ce3d18f1a6851c
Adalbert Lazăr <alazar@bitdefender.com>
2018-01-04 18:07:07 +0200

    tools/kvm/kvmi/libkvmi: change the license to LGPL 3
    This should make the code compatible with the libvmi project.
    
    Signed-off-by: Mihai Donțu <mdontu@bitdefender.com>
    Signed-off-by: Adalbert Lazăr <alazar@bitdefender.com>
    Signed-off-by: Nicușor Cîțu <ncitu@bitdefender.com>
    Signed-off-by: Mircea Cîrjaliu <mcirjaliu@bitdefender.com>
    Signed-off-by: Marian Rotariu <mrotariu@bitdefender.com>

--- tools/kvm/kvmi/include/kvmi/libkvmi.h
+++ tools/kvm/kvmi/include/kvmi/libkvmi.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2017 Bitdefender S.R.L.
+ * Copyright (C) 2017-2018 Bitdefender S.R.L.
  *
  * The KVMI Library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
  * The KVMI Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
