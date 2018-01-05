7cdcee8ff13be2eaf03ddfd2a4906defeafe45e7
Adalbert Lazăr <alazar@bitdefender.com>
2018-01-05 17:05:30 +0200

    tools/kvm/kvmi/libkvmi: avoid unlink() errors when the unix socket doesn't exist
    Signed-off-by: Adalbert Lazăr <alazar@bitdefender.com>

--- tools/kvm/kvmi/libkvmi.c
+++ tools/kvm/kvmi/libkvmi.c
@@ -32,6 +32,9 @@
 #include <poll.h>
 #include <kvmi/libkvmi.h>
 #include <linux/kvm_para.h>
+#ifdef USE_UNIX_SOCKET
+#include <sys/stat.h>
+#endif
 
 #ifndef __unused
 #ifdef __GNUC__
@@ -100,11 +103,12 @@ static int setup_socket( void )
 	int         pf      = PF_UNIX;
 	size_t      sa_size = sizeof( sock_addr );
 	const char *path    = getenv( "LIBKVMI_SOCKET" );
+	struct stat st;
 
 	if ( !path || path[0] == 0 )
 		return -1;
 
-	if ( unlink( path ) ) /* Address already in use */
+	if ( stat( path, &st ) == 0 && unlink( path ) ) /* Address already in use */
 		return -1;
 
 	sock_addr.sun_family = AF_UNIX;
