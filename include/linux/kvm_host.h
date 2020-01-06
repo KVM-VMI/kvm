45eb0f0cfd51b2f2fde965acbe7070cbaa9e281f
Nicușor Cîțu <ncitu@bitdefender.com>
2020-01-06 19:02:25 +0200

    KVM: x86: export kvm_arch_vcpu_set_guest_debug()
    This function is needed for the KVMI_EVENT_BP event.
    
    Signed-off-by: Nicușor Cîțu <ncitu@bitdefender.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -855,6 +855,8 @@ int kvm_arch_vcpu_ioctl_set_mpstate(struct kvm_vcpu *vcpu,
 				    struct kvm_mp_state *mp_state);
 int kvm_arch_vcpu_ioctl_set_guest_debug(struct kvm_vcpu *vcpu,
 					struct kvm_guest_debug *dbg);
+int kvm_arch_vcpu_set_guest_debug(struct kvm_vcpu *vcpu,
+				  struct kvm_guest_debug *dbg);
 int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *kvm_run);
 
 int kvm_arch_init(void *opaque);
