e0ef95a928d7bc51a9d49b29f663d860a7b79909
Nicușor Cîțu <ncitu@bitdefender.com>
2020-01-08 16:48:14 +0200

    KVM: x86: export kvm_vcpu_ioctl_x86_get_xsave()
    This function is needed for the KVMI_VCPU_GET_XSAVE command.
    
    Signed-off-by: Nicușor Cîțu <ncitu@bitdefender.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -858,6 +858,8 @@ int kvm_arch_vcpu_ioctl_set_guest_debug(struct kvm_vcpu *vcpu,
 int kvm_arch_vcpu_set_guest_debug(struct kvm_vcpu *vcpu,
 				  struct kvm_guest_debug *dbg);
 int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *kvm_run);
+void kvm_vcpu_ioctl_x86_get_xsave(struct kvm_vcpu *vcpu,
+				  struct kvm_xsave *guest_xsave);
 
 int kvm_arch_init(void *opaque);
 void kvm_arch_exit(void);
