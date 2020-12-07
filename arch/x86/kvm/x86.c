a8a42816ad640f9d22a8162e3237fef24e8168a4
Mihai Donțu <mdontu@bitdefender.com>
2020-12-07 17:58:48 +0200

    KVM: x86: avoid injecting #PF when emulate the VMCALL instruction
    It can happened to end up emulating the VMCALL instruction as a result
    of the handling of an EPT write fault. In this situation,
    the emulator will try to unconditionally patch the correct hypercall
    opcode bytes using emulator_write_emulated(). However, this last call
    uses the fault GPA (if available) or walks the guest page tables at RIP,
    otherwise. The trouble begins when using VM introspection,
    when we forbid the use of the fault GPA and fallback to the guest pt walk:
    in Windows (8.1 and newer) the page that we try to write into
    is marked read-execute and as such emulator_write_emulated() fails
    and we inject a write #PF, leading to a guest crash.
    
    Signed-off-by: Mihai Donțu <mdontu@bitdefender.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -8144,11 +8144,15 @@ static int emulator_fix_hypercall(struct x86_emulate_ctxt *ctxt)
 	struct kvm_vcpu *vcpu = emul_to_vcpu(ctxt);
 	char instruction[3];
 	unsigned long rip = kvm_rip_read(vcpu);
+	int err;
 
 	kvm_x86_ops.patch_hypercall(vcpu, instruction);
 
-	return emulator_write_emulated(ctxt, rip, instruction, 3,
+	err = emulator_write_emulated(ctxt, rip, instruction, 3,
 		&ctxt->exception);
+	if (err == X86EMUL_PROPAGATE_FAULT)
+		err = X86EMUL_CONTINUE;
+	return err;
 }
 
 static int dm_request_for_irq_injection(struct kvm_vcpu *vcpu)
