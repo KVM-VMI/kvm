6efe404b27df3d8c90855c8b00dc3eca45612947
Mihai Donțu <mdontu@bitdefender.com>
2021-10-06 19:15:24 +0259

    KVM: introspection: add KVMI_VCPU_CONTROL_MSR and KVMI_VCPU_EVENT_MSR
    This command is used to enable/disable introspection for a specific
    MSR. The KVMI_VCPU_EVENT_MSR event is sent when the tracked MSR is going
    to be changed. The introspection tool can respond by allowing the guest
    to continue with normal execution or by discarding the change.
    
    This is meant to prevent malicious changes to MSRs
    such as MSR_IA32_SYSENTER_EIP.
    
    Signed-off-by: Mihai Donțu <mdontu@bitdefender.com>
    Co-developed-by: Nicușor Cîțu <nicu.citu@icloud.com>
    Signed-off-by: Nicușor Cîțu <nicu.citu@icloud.com>
    Co-developed-by: Adalbert Lazăr <alazar@bitdefender.com>
    Signed-off-by: Adalbert Lazăr <alazar@bitdefender.com>

--- arch/x86/include/uapi/asm/kvmi.h
+++ arch/x86/include/uapi/asm/kvmi.h
@@ -141,4 +141,22 @@ struct kvmi_vcpu_event_descriptor {
 	__u8 padding[6];
 };
 
+struct kvmi_vcpu_control_msr {
+	__u8 enable;
+	__u8 padding1;
+	__u16 padding2;
+	__u32 msr;
+};
+
+struct kvmi_vcpu_event_msr {
+	__u32 msr;
+	__u32 padding;
+	__u64 old_value;
+	__u64 new_value;
+};
+
+struct kvmi_vcpu_event_msr_reply {
+	__u64 new_val;
+};
+
 #endif /* _UAPI_ASM_X86_KVMI_H */
