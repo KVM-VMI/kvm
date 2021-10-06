dab2f8e67305eec8f0817d6bd2d5237e2c498a7f
Mihai Donțu <mdontu@bitdefender.com>
2021-10-06 19:15:00 +0259

    KVM: x86: extend kvm_mmu_gva_to_gpa_system() with the 'access' parameter
    This is needed for kvmi_update_ad_flags() to emulate a guest page
    table walk on SPT violations due to A/D bit updates.
    
    Signed-off-by: Mihai Donțu <mdontu@bitdefender.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1788,7 +1788,7 @@ gpa_t kvm_mmu_gva_to_gpa_fetch(struct kvm_vcpu *vcpu, gva_t gva,
 gpa_t kvm_mmu_gva_to_gpa_write(struct kvm_vcpu *vcpu, gva_t gva,
 			       struct x86_exception *exception);
 gpa_t kvm_mmu_gva_to_gpa_system(struct kvm_vcpu *vcpu, gva_t gva,
-				struct x86_exception *exception);
+				u32 access, struct x86_exception *exception);
 
 bool kvm_apicv_activated(struct kvm *kvm);
 void kvm_vcpu_update_apicv(struct kvm_vcpu *vcpu);
