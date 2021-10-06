d33950e0d66420de3d1da9826953ffa5a68c4be2
Mihai Donțu <mdontu@bitdefender.com>
2021-10-06 19:15:15 +0259

    KVM: introspection: add KVMI_VCPU_GET_REGISTERS
    This command is used to get kvm_regs and kvm_sregs structures,
    plus a list of struct kvm_msrs from a specific vCPU.
    
    While the kvm_regs and kvm_sregs structures are included with every
    event, this command allows reading any MSR.
    
    Signed-off-by: Mihai Donțu <mdontu@bitdefender.com>
    Co-developed-by: Adalbert Lazăr <alazar@bitdefender.com>
    Signed-off-by: Adalbert Lazăr <alazar@bitdefender.com>

---
+++ arch/x86/kvm/kvmi.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef ARCH_X86_KVM_KVMI_H
+#define ARCH_X86_KVM_KVMI_H
+
+int kvmi_arch_cmd_vcpu_get_registers(struct kvm_vcpu *vcpu,
+				const struct kvmi_vcpu_get_registers *req,
+				struct kvmi_vcpu_get_registers_reply *rpl);
+
+#endif
