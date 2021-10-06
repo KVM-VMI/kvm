0417a30f12dd7c658609946dd681a98ffc0e1b3a
Nicușor Cîțu <nicu.citu@icloud.com>
2021-10-06 19:15:18 +0259

    KVM: introspection: restore the state of #BP interception on unhook
    This commit also ensures that only the userspace or the introspection
    tool can control the #BP interception exclusively at one time.
    
    Signed-off-by: Nicușor Cîțu <nicu.citu@icloud.com>

--- arch/x86/include/asm/kvmi_host.h
+++ arch/x86/include/asm/kvmi_host.h
@@ -4,8 +4,15 @@
 
 #include <asm/kvmi.h>
 
+struct kvmi_monitor_interception {
+	bool kvmi_intercepted;
+	bool kvm_intercepted;
+	bool (*monitor_fct)(struct kvm_vcpu *vcpu, bool enable);
+};
+
 struct kvmi_interception {
 	bool restore_interception;
+	struct kvmi_monitor_interception breakpoint;
 };
 
 struct kvm_vcpu_arch_introspection {
@@ -15,5 +22,16 @@ struct kvm_vcpu_arch_introspection {
 
 struct kvm_arch_introspection {
 };
+
+#ifdef CONFIG_KVM_INTROSPECTION
+
+bool kvmi_monitor_bp_intercept(struct kvm_vcpu *vcpu, u32 dbg);
+
+#else /* CONFIG_KVM_INTROSPECTION */
+
+static inline bool kvmi_monitor_bp_intercept(struct kvm_vcpu *vcpu, u32 dbg)
+	{ return false; }
+
+#endif /* CONFIG_KVM_INTROSPECTION */
 
 #endif /* _ASM_X86_KVMI_HOST_H */
